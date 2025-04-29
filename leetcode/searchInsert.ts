8830f6d41dcd93f6ae4bc8b6f574932ed042139f
Xustavo <gugaxavier2005@gmail.com>
2025-04-29 16:46:48 -0300

    update: extra challenge

---
+++ leetcode/searchInsert.ts
@@ -0,0 +1,13 @@
+function searchInsert(nums: number[], target: number) {
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] === target){
+            return i;
+        } else if(target > nums[i] && target < nums[i+1]){
+            return i+1;
+        } else if(target > nums[nums.length-1]){
+            return nums.length;
+        } else if(target < nums[i]) {
+            return 0;
+        }
+    }
+};
