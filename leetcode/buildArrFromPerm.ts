6320f6834efcd8688289ec2278c798c3064357b6
Gustavo Xavier <157379254+Xustavo@users.noreply.github.com>
2025-05-14 20:23:55 +0000

    update: add daily challenge

---
+++ leetcode/buildArrFromPerm.ts
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/build-array-from-permutation/
+
+function buildArray(nums: number[]): number[] {
+    let ans: number[] = [];
+
+    for(let i = 0; i < nums.length; i++){
+        ans[i] = nums[nums[i]];
+    }
+
+    return ans;
+};
