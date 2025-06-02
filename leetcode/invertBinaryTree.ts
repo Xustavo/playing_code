205cd5657c39ccdac662540874423de91c97d95a
Gustavo Xavier <157379254+Xustavo@users.noreply.github.com>
2025-06-02 21:19:11 +0000

    update: add new challenge

---
+++ leetcode/invertBinaryTree.ts
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/invert-binary-tree
+
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.left = (left===undefined ? null : left)
+        this.right = (right===undefined ? null : right)
+    }
+ }
+ 
+
+function invertTree(root: TreeNode | null): TreeNode | null {
+    if(!root) return null;
+    const left = root.left;
+    root.left = invertTree(root.right);
+    root.right = invertTree(left);
+    return root;
+};
